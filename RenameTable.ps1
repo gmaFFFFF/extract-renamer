910005e38da4ab5469f7105425e452a68c7863de
Maksim Grishkin <FFFFF@bk.ru>
2019-04-02 18:07:50 +0300

    feat(core): Добавляет таблицу переименований выписок
    Таблица переименований -- это файл в котором указаны:
    . текущий путь к файлу выписки (xml);
    . текущий путь ко всем вспомогательным файлам (ЭЦП и человекочитаемый формат);
    . расчетный (проектный) путь к вышеуказанным файлам.

---
+++ RenameTable.ps1
@@ -0,0 +1,36 @@
+. .\ExtractReader.ps1
+
+
+function Generate-ExtractRenameTable($rosreestrExtracts)
+{
+    [array]$RenameTable = @()
+    foreach ($extract in $rosreestrExtracts)
+    {      
+        [string] $extrClass = Get-ExtractClass($extract)
+        [DateTime] $extrDate = Get-ExtractDate($extract)
+        [string] $extrCadNum = (Get-ExtractCadNum($extract))
+                
+        [string] $fileName = $extract.Name
+        [string] $name = [io.path]::GetFileNameWithoutExtension($extract.Name)
+     
+        [string] $newName = "{2}_{1}_{0}" -f ($extrDate.ToString("yyyy-MM-dd")),$extrCadNum.Replace(":","-"),$extrClass
+        
+        [array] $allFile = Get-ExtractAuxFile($extract)
+        $allFile += $extract
+
+        foreach($file in $allFile)
+        {
+            $rec = [PSCustomObject]@{
+                    Path=$file.FullName
+                    NewName = $file.Name.Replace($name,$newName)
+                    CadNum = $extrCadNum
+                    ClassExtr = $extrClass
+                    Date = $extrDate
+            }
+            $RenameTable += $rec
+        }
+    }
+    return $RenameTable
+}
+
+
