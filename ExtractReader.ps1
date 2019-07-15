c9cd6219e5306833c299bce1e627aa5cdf04b84b
Maksim Grishkin <FFFFF@bk.ru>
2019-07-15 19:30:54 +0300

    feat(parser): Добавляет поддержку дополнительных типов xml выписок

--- ExtractReader.ps1
+++ ExtractReader.ps1
@@ -4,24 +4,49 @@ function Get-ExtractCadNum ($rosreestrExtract)
     switch ($cls){
     'KVZU' {[string] $xpath="/*[local-name() = 'KVZU']/*[local-name() = 'Parcels']/*[local-name() = 'Parcel']/@CadastralNumber"}
     'KVOKS'{[string] $xpath="/*[local-name() = 'KVOKS']/*[local-name() = 'Realty']/*[local-name() = 'Building']/@CadastralNumber"}
+    'KPT'{[string] $xpath="/*[local-name() = 'KPT']/*[local-name() = 'CadastralBlocks']/*[local-name() = 'CadastralBlock']/@CadastralNumber"}
+    'CadastralCostDoc'{[string] $xpath="/*[local-name() = 'CadastralCostDoc']/*[local-name() = 'Object']/@CadastralNumber"}
+    'KPZU'{[string] $xpath="/*[local-name() = 'KPZU']/*[local-name() = 'Parcel']/@CadastralNumber"}
+    'extract_cadastral_plan_territory'{[string] $xpath="/extract_cadastral_plan_territory/cadastral_blocks/cadastral_block/cadastral_number/text()"}
+    'extract_base_params_land'{[string] $xpath="/extract_base_params_land/land_record/object/common_data/cad_number/text()"}
+    'extract_base_params_build'{[string] $xpath="/extract_base_params_build/build_record/object/common_data/cad_number/text()"}    
     }
-    [string] $cn = (Select-Xml $rosreestrExtract.FullName -Xpath $xpath).Node.Value    
+    [string] $cn = (Select-Xml -LiteralPath $rosreestrExtract.FullName -Xpath $xpath).Node.Value    
     return $cn
 }
 
 function Get-ExtractDate ($rosreestrExtract)
 {
-    [string] $xpath="//*[local-name() = 'ReestrExtract']/*[local-name() = 'DeclarAttribute']/@ExtractDate"
-    [string] $dateStr = (Select-Xml $rosreestrExtract.FullName -Xpath $xpath).Node.Value
-    [DateTime] $date = [datetime]::ParseExact($dateStr,"dd.MM.yyyy",[Globalization.CultureInfo]::CreateSpecificCulture('ru-RU'))
-        
+    [string] $cls = Get-ExtractClass ($rosreestrExtract)
+    switch ($cls){    
+    {$_ -in 'extract_base_params_land', 'extract_base_params_build', 'extract_cadastral_plan_territory'}`
+        {[string] $xpath="/*/details_statement/group_top_requisites/date_formation/text()"}
+    {$_ -in 'KPT', 'CadastralCostDoc'}`
+        {[string] $xpath="/*/*[local-name() = 'CertificationDoc']/*[local-name() = 'Date']/text()"}    
+    Default`
+        {[string] $xpath="//*[local-name() = 'ReestrExtract']/*[local-name() = 'DeclarAttribute']/@ExtractDate"}
+    }
+    
+    [string] $dateStr = (Select-Xml -LiteralPath $rosreestrExtract.FullName -Xpath $xpath).Node.Value
+
+
+    switch ($cls){
+    {$_ -in 'extract_cadastral_plan_territory', 'extract_base_params_land', 'KPT', 'extract_base_params_build'} `
+        {[DateTime] $date = [datetime]::ParseExact($dateStr,"yyyy-MM-dd",[Globalization.CultureInfo]::CreateSpecificCulture('ru-RU'))}
+    Default`
+        {[DateTime] $date = [datetime]::ParseExact($dateStr,"dd.MM.yyyy",[Globalization.CultureInfo]::CreateSpecificCulture('ru-RU'))}
+    }
+
+
     return $date
 }
 
 function Get-ExtractClass ($rosreestrExtract)
 {    
-    [string] $xpath="(//*)[1]"
-    [string] $cls = (Select-Xml -Path $rosreestrExtract.FullName -Xpath $xpath).Node.LocalName 
+    [string] $xpath = "/*"
+    
+    [string] $cls = (Select-Xml -LiteralPath $rosreestrExtract.FullName -Xpath $xpath).Node.LocalName
+
 
     return $cls
 }
@@ -30,7 +55,15 @@ function Get-ExtractClass ($rosreestrExtract)
 function Get-ExtractAuxFile ($rosreestrExtract)
 {
     [string] $fileName = [io.path]::GetFileNameWithoutExtension($rosreestrExtract.Name)
-    [string] $folder = $rosreestrExtract.Directory.FullName + "\*"
-    [array] $auxFiles = @(Get-ChildItem $folder -Filter ($fileName+"*")  -Include ($fileName+".xml.sig"), ($fileName+".pdf"), ($fileName+".pdf.sig"), ($fileName+".xps"))
+    $fileName = [Management.Automation.WildcardPattern]::Escape($fileName)
+    [string] $folder = [Management.Automation.WildcardPattern]::Escape($rosreestrExtract.Directory.FullName) + "\*"
+
+        [array] $auxFiles = @(Get-ChildItem -Path $folder `
+                        -Include ($fileName+".xml.original.sig"), `
+                                 ($fileName+".xml.original"), `
+                                 ($fileName+".xml.sig"), `
+                                 ($fileName+".pdf"), `
+                                 ($fileName+".pdf.sig"), `
+                                 ($fileName+".xps"))
     return $auxFiles
 }
