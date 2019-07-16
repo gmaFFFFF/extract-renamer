0a12b68a0771f75219e25a353c75bcae319c1763
Maksim Grishkin <FFFFF@bk.ru>
2019-07-16 19:01:55 +0300

    fix(cli): Исправляет обработку файлов с подстановочными знаками, например, [

--- main.ps1
+++ main.ps1
@@ -1,10 +1,11 @@
 . .\RenameTable.ps1
 
-[string] $path = $MyInvocation.MyCommand.Path | Split-Path -Parent
+# [string] $path = $MyInvocation.MyCommand.Path | Split-Path -Parent
+[string] $path = "\\msk-gis01\MSSQLSERVER\Doc\RosreestrInbox\ฮสั"
 [string] $renTableName = $path + "\renameTable.csv"
 
 if (Test-Path -Path $renTableName){
-    Import-Csv ($renTableName) -Encoding UTF8 | Rename-Item
+    Import-Csv ($renTableName) -Encoding UTF8 | foreach { Rename-Item -LiteralPath $_.Path -NewName $_.NewName}
     Remove-Item $renTableName -Force
 }
 else
