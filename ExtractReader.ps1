b13091148e1ea018f77526875616499bb42354e4
Maksim Grishkin <fffff@bk.ru>
2022-04-19 13:16:04 +0300

    feat(parser): Добавляет поддержку xml выписок типа 'extract_about_property_under_construction'

--- ExtractReader.ps1
+++ ExtractReader.ps1
@@ -18,7 +18,7 @@ function Get-ExtractCadNum ($rosreestrExtract)
     {$_ -in 'extract_base_params_land', 'extract_base_params_build','extract_base_params_construction', 'extract_about_property_land', 
 			'extract_about_property_construction', 'extract_about_property_build', 'extract_cadastral_value_property', 
 			'extract_transfer_rights_property', 'extract_base_params_room', 'extract_base_params_under_construction', 
-            'extract_about_property_room'} `
+            'extract_about_property_under_construction', 'extract_about_property_room'} `
         {[string] $xpath="(//object/common_data/cad_number)[1]/text()"}
     
     }
@@ -34,7 +34,7 @@ function Get-ExtractDate ($rosreestrExtract)
     {$_ -in 'extract_base_params_build', 'extract_base_params_land','extract_base_params_construction', 'extract_cadastral_plan_territory', 
 				'extract_about_property_land', 'extract_about_property_construction', 'extract_about_property_build', 
 				'extract_cadastral_value_property', 'extract_transfer_rights_property', 'extract_base_params_room',
-                'extract_base_params_under_construction', 'extract_about_property_room'}`
+                'extract_base_params_under_construction', 'extract_about_property_under_construction', 'extract_about_property_room'}`
         {[string] $xpath="/*/details_statement/group_top_requisites/date_formation/text()"}
 
     {$_ -in 'Region_Cadastr', 'Region_Cadastr_Vidimus_KP', 'Region_Cadastr_Vidimus_KV','CadastralCostDoc', 'KPOKS', 'KP_OKS', 'KPZU', 'KVZU', 'KPT', 'KVOKS'}`
